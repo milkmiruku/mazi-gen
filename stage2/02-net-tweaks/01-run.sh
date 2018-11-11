12ce3c92c14cf6f48c1c0da15229350f5d7632ff
Milk Brewster <github@milkmiruku.com>
2018-11-11 05:14:12 +0000

    Second pass at a Mazi setup

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -11,8 +11,12 @@ install -v -m 600 files/dnsmasq.conf	"${ROOTFS_DIR}/etc/"
 install -v -d					"${ROOTFS_DIR}/etc/hostapd"
 install -v -m 600 files/hostapd.conf	"${ROOTFS_DIR}/etc/hostapd/"
 
+install -v -m 600 files/index.html	"${ROOTFS_DIR}/var/www/html/"
+install -v -m 600 files/.htaccess	"${ROOTFS_DIR}/var/www/html/"
+
 on_chroot << EOF
 systemctl enable dnsmasq
-sed -i '/^#.* ip_forward /s/^#//' 
+sed -i '/^#.* ip_forward /s/^#//' /etc/sysctl.conf
+a2enmod rewrite
+sed -i '/^#.* AllowOverride None /s/None/all/' /etc/apache2/apache2.conf
 EOF
-
--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -5,5 +5,25 @@ update-alternatives --install /usr/bin/x-www-browser \
   x-www-browser /usr/bin/chromium-browser 86
 update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
+
 pip install speedtest-cli
+
+curl -sL https://deb.nodesource.com/setup_11.x | bash -
+apt-get install -y nodejs
+
+mysql -u root -p
+CREATE DATABASE etherpad CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL PRIVILEGES ON etherpad.* TO etherpad_user@localhost IDENTIFIED BY ‘password’;
+FLUSH PRIVILEGES;
+quit
+
+cd /var/www/html
+git clone git://github.com/ether/etherpad-lite.git
+cd etherpad-lite
+cp settings.json.template settings.json
+#todo
+
+adduser --system --home=/var/www/html/etherpad-lite/ --group etherpad
+chown -R etherpad: /var/www/html/etherpad-lite/
+bash /var/www/html/etherpad-lite/bin/run.sh
 EOF
