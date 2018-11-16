bc61d015b224c246f45a8c7ccf5543a0f8619e84
Milk Brewster <github@milkmiruku.com>
2018-11-16 07:39:08 +0000

    mysql to mariadb and fix stage3 install

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -8,38 +8,46 @@ update-alternatives --install /usr/bin/gnome-www-browser \
 
 pip install speedtest-cli
 
+
+# for etherpad
 curl -sL https://deb.nodesource.com/setup_11.x | bash -
 apt-get install -y nodejs
 
-mysql -u root -p
-CREATE DATABASE etherpad CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON etherpad.* TO etherpad_user@localhost IDENTIFIED BY ‘password’;
-FLUSH PRIVILEGES;
-quit
+/etc/init.d/mysql start
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS etherpad CHARACTER SET utf8 COLLATE utf8_general_ci; GRANT ALL PRIVILEGES ON etherpad.* TO etherpad_user@localhost IDENTIFIED BY 'password'; FLUSH PRIVILEGES;"
 
 cd /var/www/html
-git clone git://github.com/ether/etherpad-lite.git
+mkdir -p etherpad-lite
 cd etherpad-lite
+git init
+git remote set-url origin git://github.com/ether/etherpad-lite.git
+git fetch
+git checkout origin/master -f
 cp settings.json.template settings.json
 #todo
 
 adduser --system --home=/var/www/html/etherpad-lite/ --group etherpad
 chown -R etherpad: /var/www/html/etherpad-lite/
-bash /var/www/html/etherpad-lite/bin/run.sh
+# bash /var/www/html/etherpad-lite/bin/run.sh
 
 
-# for portal
+# for Mazi portal
 gem install sinatra sequel sqlite3 rake thin rubyzip mysql --no-ri --no-rdoc
 
 cd /root
-git clone https://github.com/mazi-project/portal.git
-
+mkdir -p portal
 cd portal
+git init
+git remote set-url origin https://github.com/mazi-project/portal.git
+git fetch
+git checkout origin/master -f
+
 rake init
 rake db:migrate
 
 cp init/mazi-portal /etc/init.d/mazi-portal
 chmod +x /etc/init.d/mazi-portal
+update-rc.d mazi-portal defaults
 update-rc.d mazi-portal enable
 
 # data collection service
